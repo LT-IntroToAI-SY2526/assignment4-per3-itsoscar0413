96528aa90e72cc6b4b4f083f972640080280e21a
Oscar David huerta <odhuerta@cps.edu>
2025-10-23 10:23:49 -0500

    took notes, and added sleep function

--- notes.py
+++ notes.py
@@ -2,3 +2,58 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+# a class is a blueprint, describing what it is, while an object is what makes it unique/individual with specific traits
+
+class Dog:
+    """
+    a simple dog class to learn object oriented program
+
+    attribute:
+        fur_col: color of the fur
+        name: the name of the dog
+        age: how old a dog is
+        favorite_food: the favorite food of the dog
+    """
+    def __init__(self, fur_color, name, age, favorite_food): # self word means "this" (current object)
+        """initialize a new Dog with fur_color, name, age and favorite food"""
+        self.fur_color = fur_color
+        self.name = name
+        self.age = age
+        self.favorite_food = favorite_food
+
+    def __str__(self):
+        """this is going to return a string representation of a Dog"""
+        return f"{self.name} is a {self.age} year old {self.fur_color} dog who loves {self.favorite_food}"
+    
+    def bark(self):
+        """make the dog bark"""
+        # user-made function
+        return f"{self.name} says Woof, Woof!!"
+    
+    def sleep(self):
+        """practicing, to make the dog sleep"""
+        return f"{self.name} went to sleep! Sweet dreams :)"
+
+
+
+my_dog = Dog("black", "logan", 9, "salmon")
+enggy_dog = Dog("black and white", "petuchini", 13, "rice")
+random_dog = Dog("yellow", "bennyboy", 3, "kibble")
+
+print()
+
+print(my_dog)
+print(enggy_dog)
+print(random_dog)
+
+print()
+
+print(enggy_dog.bark())
+print(my_dog.bark())
+
+print()
+
+print(enggy_dog.sleep())
+print(random_dog.sleep())
+
+print()
